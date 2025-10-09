c582344c82ea6513ac3931a9b90291f06ff92e74
flowerinthenight <root@flowerinthenight.com>
2025-10-10 00:05:37 +0900

    ci skip: add note on test VM

--- localbuild.sh
+++ localbuild.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# NOTE on test VM:
+# gcloud compute instances create bpf-kvm \
+#        --enable-nested-virtualization \
+#        --zone asia-northeast1-b \
+#        --min-cpu-platform "AUTOMATIC" \
+#        --machine-type n2-standard-4 \
+#        --image-project ubuntu-os-cloud \
+#        --image-family ubuntu-2404-lts-amd64 \
+#        --boot-disk-size 50 \
+#        --project alphaus-dashboard
+
 # Local build only; environment specific.
 go generate
 DOCKER_BUILDKIT=0 docker build --rm -t vortex-agent .
